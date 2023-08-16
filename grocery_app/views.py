eb5e26a3c322be0adf9d872161c042801786e425
vrajsoni98 <vrajsoni727@gmail.com>
2023-08-16 17:02:28 -0400

    code optimize

--- grocery_app/views.py
+++ grocery_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import generics, status
-from rest_framework.response import Response
+from rest_framework import generics
 from .models import GroceryList, GroceryItem
 from .serializers import GroceryListSerializer, GroceryItemSerializer
 
